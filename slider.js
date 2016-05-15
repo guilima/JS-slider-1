7c748e55004311f67b4b01981befdb3b2da61f1a
Guilherme Carvalho Lima <gui.lima30@gmail.com>
2016-05-15 13:35:30 -0300

    Add autoplayer fuction

--- slider.js
+++ slider.js
@@ -1,10 +1,10 @@
-var slideItem = document.querySelectorAll(".slides li"),
-		itemLen = slideItem.length,
-		slide = slideItem[0].parentElement,
-		setElem = slide.parentElement,
-		navBtn = document.querySelectorAll(".flex-control-nav a"),
-		currentItem = 0,
-		screenW= setElem.offsetWidth;
+var slideItem = document.querySelectorAll(".slides li:not(.clone)"),
+	itemLen = slideItem.length,
+	slide = slideItem[0].parentElement,
+	setElem = slide.parentElement,
+	navBtn = document.querySelectorAll(".slides-control-nav a"),
+	currentItem = 0,
+	screenW= setElem.offsetWidth;
 
 function onWindowResize() { 
 	screenW = setElem.offsetWidth;
@@ -17,6 +17,7 @@ function onWindowResize() {
 }
 
 function setSize() { 
+	slideItem = document.querySelectorAll(".slides li");
 	for (var i = 0; i < itemLen; i++) {
 		slideItem[i].style.width = screenW + 'px';
 	}
@@ -40,6 +41,31 @@ function navMoves() {
 	}
 }
 
+function autoPlay() {
+	currentItem < itemLen ? currentItem++ : currentItem = 1;
+	resizedItem = currentItem * screenW;
+	
+	for (var i = 0; i < itemLen; i++) {
+		navBtn[i].style.background = '';		
+	}	
+	if (currentItem == itemLen) {
+		navBtn[0].style.background = 'red';
+	} else {
+		navBtn[currentItem].style.background = 'red';
+	}
+	slide.style.transition = '0.35s ease-out';
+	slide.style.transform = "translate3d(-"+resizedItem+"px, 0px, 0px)";
+}
+
+function transitionEnd(e) {
+	if(currentItem== itemLen){
+		e.target.style.transition = '0s ease-out';
+		e.target.style.transform = "translate3d(0px, 0px, 0px)";
+	}
+}
+
 navMoves();
 setSize();
 window.addEventListener( 'resize',onWindowResize, false);
+var intervalID = window.setInterval(autoPlay, 5500);
+slide.addEventListener("transitionend", transitionEnd, false);
